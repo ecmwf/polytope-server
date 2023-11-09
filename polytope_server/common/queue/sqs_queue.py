32421383d8d6140c671d8f42c3ad5ef477267723
milosbeliczuhlke <milos.belic@zuehlke.com>
2023-11-09 12:04:23 +0100

    Add SQS queue

---
+++ polytope_server/common/queue/sqs_queue.py
@@ -0,0 +1,99 @@
+import json
+import logging
+from . import queue
+import boto3
+
+class SQSQueue(queue.Queue):
+    def __init__(self, config):
+        
+        host = config.get("host", "localhost")
+        queue_name = config.get("name", "default")
+        self.username = config.get("user", "guest")
+        self.password = config.get("password", "guest")
+        self.keep_alive_interval = config.get("keep_alive_interval", 60)
+        self.queue_url = "{}/{}".format(host, queue_name)
+        
+        logging.getLogger("sqs").setLevel("WARNING")
+        
+        self.client = boto3.client('sqs')
+        self.check_connection()
+
+    def enqueue(self, message):
+        self.client.send_message(
+        QueueUrl=self.queue_url,
+        MessageBody=json.dumps(message.body).encode("utf-8")
+        )
+
+    def dequeue(self):
+        response = self.client.receive_message(
+            QueueUrl=self.queue_url,
+            VisibilityTimeout=120, #If processing takes more seconds, message will be read twice
+            MaxNumberOfMessages=1,
+            )
+        if not response['Messages']:
+            return None
+        if len(response['Messages']) > 1:
+            raise ValueError("Received {} messages, should have received 1".format(len(response['Messages'])))
+
+        body = response['Messages'][0]['Body']   
+        receipt_handle = response['Messages'][0]['ReceiptHandle']    
+         
+        return queue.Message(json.loads(body.decode("utf-8")), context=receipt_handle)
+
+
+    def ack(self, message):
+        self.client.delete_message(
+            QueueUrl=self.queue_url,
+            ReceiptHandle=message.context
+        )
+
+    def nack(self, message):
+        self.client.change_message_visibility(
+            QueueUrl=self.queue_url,
+            ReceiptHandle=message.context,
+            VisibilityTimeout=0
+        )
+
+    def keep_alive(self):
+        return self.check_connection()
+
+    def check_connection(self):
+        """Check the queue connection"""
+        response = self.client.get_queue_attributes(
+            QueueUrl=self.queue_url,
+            AttributeNames=['CreatedTimestamp']
+        )
+        #Tries to parse response
+        return 'Attributes' in response & 'CreatedTimestamp' in response['Attributes']
+
+    def close_connection(self):
+        self.client.close()
+
+    def count(self):
+        response = self.client.get_queue_attributes(
+            QueueUrl=self.queue_url,
+            AttributeNames=[
+                'ApproximateNumberOfMessages'
+            ]
+        )
+        num_messages = response['Attributes']['ApproximateNumberOfMessages']
+        
+        return int(num_messages)
+
+    def get_type(self):
+        return "sqs"
+
+    def collect_metric_info(self):
+        response = self.client.get_queue_attributes(
+            QueueUrl=self.queue_url,
+            AttributeNames=[
+                'ApproximateNumberOfMessages',
+                'ApproximateNumberOfMessages',
+                'ApproximateNumberOfMessagesNotVisible',
+            ]
+        )
+        return response['Attributes']
+        
+        
+
+        
