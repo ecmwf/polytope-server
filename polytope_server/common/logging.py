5ec1e626c0e5f3ffe23a6c6c9a6a5bc2f5f64257
sametd <samet.demir@ecmwf.int>
2024-08-07 13:40:56 +0000

    enhanced logging to be more verbose

--- polytope_server/common/logging.py
+++ polytope_server/common/logging.py
@@ -21,14 +21,54 @@
 import datetime
 import json
 import logging
+import socket
 
 indexable_fields = {"request_id": str}
 
 
+class LogFormatter(logging.Formatter):
+    def __init__(self, mode):
+        super(LogFormatter, self).__init__()
+        self.mode = mode
+
+    def format(self, record):
+        # timezone-aware datetime object
+        utc_time = datetime.datetime.fromtimestamp(record.created, datetime.timezone.utc)
+        formatted_time = utc_time.strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
+
+        result = {
+            "asctime": formatted_time,
+            "hostname": getattr(record, "hostname", socket.gethostname()),
+            "process": record.process,
+            "thread": record.thread,
+            "name": record.name,
+            "filename": record.filename,
+            "lineno": record.lineno,
+            "levelname": record.levelname,
+            "message": record.getMessage(),
+        }
+
+        if self.mode == "console":
+            return result["asctime"] + " | " + result["message"]
+        else:
+            # following adds extra fields to the log message
+            for name, typ in indexable_fields.items():
+                if hasattr(record, name):
+                    val = getattr(record, name)
+                    if isinstance(val, typ):
+                        result[name] = val
+                    else:
+                        raise TypeError("Extra information with key {} is expected to be of type {}".format(name, typ))
+            if self.mode == "logserver":
+                # Ensuring single line output
+                return json.dumps(result, indent=None)
+            elif self.mode == "prettyprint":
+                return json.dumps(result, indent=2)
+            else:
+                return json.dumps(result, indent=0)
+
+
 def setup(config, source_name):
-
-    # Override the default logger
-
     logger = logging.getLogger()
     logger.name = source_name
     handler = logging.StreamHandler()
@@ -42,42 +82,3 @@ def setup(config, source_name):
     logger.setLevel(level)
 
     logger.info("Logging Initialized")
-
-
-class LogFormatter(logging.Formatter):
-    def __init__(self, mode):
-        super(LogFormatter, self).__init__()
-        self.mode = mode
-
-    def format(self, record):
-
-        msg = super(LogFormatter, self).format(record)
-
-        result = {}
-        result["ts"] = datetime.datetime.utcnow().isoformat()[:-3] + "Z"
-        result["src"] = str(record.name)
-        result["lvl"] = str(record.levelname)
-        result["pth"] = str("{}:{}".format(record.pathname, record.lineno))
-        result["msg"] = str(msg)
-
-        # log accepts extra={} args to eg. logging.debug
-        # if the extra arguments match known indexable_fields these are added to the log
-        # these strongly-typed fields can be used for indexing of logs
-
-        if self.mode == "console":
-            return result["ts"] + " | " + result["msg"]
-
-        else:
-            for name, typ in indexable_fields.items():
-                if hasattr(record, name):
-                    val = getattr(record, name)
-                    if isinstance(val, typ):
-                        result[name] = val
-                    else:
-                        raise TypeError("Extra information with key {} is expected to be of type {}".format(name, typ))
-
-            if self.mode == "prettyprint":
-                indent = 2
-            else:
-                indent = 0
-            return json.dumps(result, indent=indent, sort_keys=True)
