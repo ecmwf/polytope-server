812c2013a6f98abfebca72ade0515ed9a688ace1
jameshawkes <James.Hawkes@ecmwf.int>
2024-09-18 22:15:05 +0000

    implement proper matching for feature extraction

--- polytope_server/common/datasource/polytope.py
+++ polytope_server/common/datasource/polytope.py
@@ -21,6 +21,7 @@
 import json
 import logging
 import os
+import copy
 
 import yaml
 from polytope.utility.exceptions import PolytopeError
@@ -35,18 +36,16 @@ class PolytopeDataSource(datasource.DataSource):
         self.type = config["type"]
         assert self.type == "polytope"
         self.match_rules = config.get("match", {})
+        self.req_single_keys = config.get("options", {}).pop("req_single_keys", [])
         self.patch_rules = config.get("patch", {})
         self.output = None
 
         # Create a temp file to store gribjump config
         self.config_file = "/tmp/gribjump.yaml"
         with open(self.config_file, "w") as f:
-            f.write(yaml.dump(self.config["gribjump_config"]))
+            f.write(yaml.dump(self.config.pop("gribjump_config")))
         self.config["datacube"]["config"] = self.config_file
         os.environ["GRIBJUMP_CONFIG_FILE"] = self.config_file
-
-        self.polytope_options = self.config.get("polytope-options", {})
-        self.polytope_mars = PolytopeMars(self.config)
 
         logging.info("Set up gribjump")
 
@@ -60,13 +59,32 @@ class PolytopeDataSource(datasource.DataSource):
         r = yaml.safe_load(request.user_request)
         logging.info(r)
 
+        # Set the "pre-path" for this request
+        pre_path = {}
+        for k,v in r.items():
+            if k in self.req_single_keys:
+                if isinstance(v, list):
+                    v = v[0]
+                pre_path[k] = v
+
+        polytope_mars_config = copy.deepcopy(self.config)
+        polytope_mars_config["options"]["pre_path"] = pre_path
+
+
+        polytope_mars = PolytopeMars(
+            polytope_mars_config,
+            log_context= {
+                "user": request.user.realm + ':' + request.user.username,
+                "id": request.id,
+            })
+        
+
         try:
-            self.output = self.polytope_mars.extract(r)
+            self.output = polytope_mars.extract(r)
             self.output = json.dumps(self.output).encode("utf-8")
         except PolytopeError as e:
-            self.output = json.dumps({"error": str(e.message)}).encode("utf-8")
-            raise e
-        # logging.info(self.output)
+            raise Exception("Polytope Feature Extraction Error: {}".format(e.message))
+
         return True
 
     def result(self, request):
@@ -76,6 +94,10 @@ class PolytopeDataSource(datasource.DataSource):
     def match(self, request):
 
         r = yaml.safe_load(request.user_request) or {}
+
+        # Check that there is a feature specified in the request
+        if "feature" not in r:
+            raise Exception("Request does not contain expected key 'feature'")
 
         for k, v in self.match_rules.items():
             # Check that all required keys exist
@@ -88,9 +110,14 @@ class PolytopeDataSource(datasource.DataSource):
             if r[k] not in v:
                 raise Exception("got {} : {}, but expected one of {}".format(k, r[k], v))
 
-            # Finally check that there is a feature specified in the request
-            if "feature" not in r:
-                raise Exception("Request does not contain expected key 'feature'")
+        # Check that there is only one value if required
+        for k, v in r.items():    
+            if k in self.req_single_keys:
+                v = [v] if isinstance(v, str) else v
+                if len(v) > 1:
+                    raise Exception("Expected only one value for key {}".format(k))
+                elif len(v) == 0:
+                    raise Exception("Expected a value for key {}".format(k))
 
     def destroy(self, request) -> None:
         pass
