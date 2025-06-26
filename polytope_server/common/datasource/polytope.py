b781461e3bccbe81c2672498e0fb20cf792c88f6
mathleur <mathilde.leuridan@ecmwf.int>
2025-06-26 17:12:25 +0200

    fix grid resolution for destine realisation 2

--- polytope_server/common/datasource/polytope.py
+++ polytope_server/common/datasource/polytope.py
@@ -310,17 +310,20 @@ def change_grids(request, config):
             return change_config_grid_res(config, res)
 
         # for activity CMIP6 and experiment hist, all models except ifs-nemo have h512 and ifs-nemo has h1024
-        if request["activity"] == "cmip6" and request["experiment"] == "hist":
-            res = 1024
+        if request["realization"] == "1":
+            if request["activity"] == "cmip6" and request["experiment"] == "hist":
+                res = 1024
 
-        # # for activity scenariomip and experiment ssp3-7.0, all models use h1024
-        # if request["activity"] == "scenariomip" and request["experiment"] == "ssp3-7.0":
-        #     res = 1024
+            # # for activity scenariomip and experiment ssp3-7.0, all models use h1024
+            # if request["activity"] == "scenariomip" and request["experiment"] == "ssp3-7.0":
+            #     res = 1024
 
-        if request["activity"] == "story-nudging":
-            res = 512
+            if request["activity"] == "story-nudging":
+                res = 512
 
-        if request["activity"] in ["baseline", "projections", "scenariomip"]:
+            if request["activity"] in ["baseline", "projections", "scenariomip"]:
+                res = 1024
+        elif request["realization"] == "2":
             res = 1024
 
     elif request.get("dataset", None) == "extremes-dt":
