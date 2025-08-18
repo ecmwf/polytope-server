6ae85a897d358a39f76701790ec88eedf0207382
Peter Tsrunchev <peter.tsrunchev@gmail.com>
2025-08-18 12:13:13 +0000

    black

--- polytope_server/common/datasource/polytope.py
+++ polytope_server/common/datasource/polytope.py
@@ -181,7 +181,11 @@ def change_grids(request, config):
             if request["realization"] == "2" and request["model"] == "ifs-fesom":
                 res = 512
 
-            if request["activity"] == "highresmip" and request["experiment"] == "cont" and request["model"] == "ifs-fesom":
+            if (
+                request["activity"] == "highresmip"
+                and request["experiment"] == "cont"
+                and request["model"] == "ifs-fesom"
+            ):
                 res = 512
 
     elif request.get("dataset", None) == "extremes-dt":
