2da7335ac5f56d9855eae94a6cfe47548b450315
Peter Tsrunchev <peter.tsrunchev@gmail.com>
2025-10-31 17:41:52 +0000

    misc: type hints

--- polytope_server/common/staging/staging.py
+++ polytope_server/common/staging/staging.py
@@ -99,7 +99,7 @@ class Staging(ABC):
         """Get url prefix for all objects (e.g. bucket name or other static URL segment)"""
 
 
-def create_staging(staging_config=None):
+def create_staging(staging_config=None) -> Staging:
 
     if staging_config is None:
         staging_config = {"polytope": {}}
