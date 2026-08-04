cda1ccb932c7a4dba2236d60ab4408f538570403
Peter Tsrunchev <peter.tsrunchev@gmail.com>
2026-08-04 09:41:27 +0000

    chore: add SPDX license headers to dynamic_grid module

--- workers/polytope-fe-worker/dynamic_grid/__main__.py
+++ workers/polytope-fe-worker/dynamic_grid/__main__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 European Centre for Medium-Range Weather Forecasts (ECMWF)
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from .service import main
 
 if __name__ == "__main__":
